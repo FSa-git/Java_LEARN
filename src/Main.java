e5aba5c5910085544bbacb20ee06359126909e7d
FS <francesco9721@gmail.com>
2022-11-16 17:04:48 +0100

    My repo for Java Tutorials

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name;
+
+        Scanner sc = new Scanner(System.in);
+
+        // Eingabe
+        System.out.println("Please enter your name down below: ");
+        name = sc.nextString();
+
+        //Printing out
+        System.out.println("Hello world!");
+    }
+}
